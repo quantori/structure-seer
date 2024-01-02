7e7ff739821d6b0733873edcad20f255a5ae2942
Membrizard <denis.sapegin@quantori.com>
2024-01-02 12:05:42 +0000

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from utils import generate_input_file, read_sdf_compounds
-
-compound = read_sdf_compounds("example_datasets/demo_compounds_qm9.sdf")[3]
-
-generate_input_file(compound=compound, save_path="test_inputs", calc_type="OPT")
